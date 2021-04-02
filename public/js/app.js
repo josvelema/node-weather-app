342a9486b1107c29e721894abdbd30e6e8223e4c
josvelema <volumensolutions@gmail.com>
2021-04-02 14:21:08 +0200

    setup env for api key

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ const form = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg1')
 const msg2 = document.querySelector('#msg2')
-
+const msg3 = document.querySelector('#msg3')
 
 
 form.addEventListener('submit', (event) => {
@@ -23,9 +23,12 @@ form.addEventListener('submit', (event) => {
     if(data.error) {
       msg1.textContent = 'error! ' + data.error;
       msg2.textContent = ''
+      msg3.textContent = ''
     } else {
       msg1.textContent = data.location + ', ' 
+      msg3.textContent = data.forecast
       msg2.textContent = data.forecast
+      
     }
   })
 })
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-  const url ='https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoiam9zMjMiLCJhIjoiY2ttdXZ2YndsMTM5MzJ3cG9tbWJpZ2V5diJ9.osBKs1_wCT5z6SF3SyUwqg&limit=1'
+  const url ='https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) + '.json?access_token=' + process.env.MAPBOX_KEY + '&limit=1'
   
   request({url, json: true},(error, {body}) => {
    
